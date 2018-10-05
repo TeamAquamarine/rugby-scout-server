fde4431a088ca5d2205d7cab275451d7d6ad7d10
Concross <cacrossley22@gmail.com>
2018-10-04 22:10:54 -0700

    propose refactor of api hello world route testing

---
+++ __tests__/src/models/coach.test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+import coach from '../../../src/models/coach';
+import mongoose from 'mongoose';
+
+describe('Coach model tests', () => {
+
+  test('test description', () => {
+    expect(1).toBe(1);
+  });
+});
