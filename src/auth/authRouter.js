9de5aecba6fc2ce45d1bdb08a3783a75170c5767
alexlhanson <alexlhanson@gmail.com>
2018-10-05 08:24:26 -0700

    Update authRouter.js

--- src/auth/authRouter.js
+++ src/auth/authRouter.js
@@ -3,13 +3,13 @@
 import User from './model';
 import express from 'express';
 
-const Authrouter = express.Router();
+const authRouter = express.Router();
 
-Authrouter.post('/register', (req, res, next) => {
+authRouter.post('/register', (req, res, next) => {
   let user = new User(req.body);
   user.save()
     .then(user => res.send(user.generateToken()))
     .catch(next);
 });
 
-export default Authrouter;
+export default authRouter;
