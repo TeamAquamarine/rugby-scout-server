e15b7131f6d1b8d02ba477d8e8487aa9bb289d07
Sharon Miller <sharon@sharonmillercreative.com>
2018-10-05 09:26:08 -0700

    set up file for team model and file to test team model

---
+++ __tests__/src/models/team.test.js
@@ -0,0 +1,4 @@
+'use strict';
+
+import Coach from '../../../src/models/coach';
+import mongoose from 'mongoose';
---
+++ src/models/team.js
@@ -0,0 +1,13 @@
+'use strict';
+
+
+import mongoose from 'mongoose';
+
+const Schema = mongoose.schema;
+
+const teamSchema = Schema ({
+
+
+});
+
+export default mongoose.model('teams', teamSchema);
