1f64ee5632bf926ae02762b887072da2e0e95d5b
alexlhanson <alexlhanson@gmail.com>
2018-10-05 12:37:04 -0700

    model tests added

--- src/auth/model.js
+++ src/auth/model.js
@@ -7,21 +7,25 @@ import bcrypt from 'bcrypt';
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  username: {type: String, required: true},
-  password: {type: String, required: true}, 
+  username: { type: String, required: true },
+  password: { type: String, required: true },
 });
 
-userSchema.pre('save', function(next){
-  bcrypt.hash(this.password, 5)
+userSchema.pre('save', function (next) {
+  this.hashPassword(5, next);
+});
+
+userSchema.methods.hashPassword = function (salt, next) {
+  bcrypt.hash(this.password, salt)
     .then(hashedPassword => {
       this.password = hashedPassword;
       next();
     })
-    .catch(error => {throw error;});
-});
+    .catch(error => { throw error; });
+};
 
 userSchema.methods.generateToken = function () {
-  return jwt.sign({id:this._id}, process.env.SECRET);
+  return jwt.sign({ id: this._id }, process.env.SECRET);
 };
 
 export default mongoose.model('users', userSchema);
