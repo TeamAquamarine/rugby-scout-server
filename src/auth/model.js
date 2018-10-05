fc965be941f05d4cf3a8f905cf90dbcb1c651284
alexlhanson <alexlhanson@gmail.com>
2018-10-05 14:29:48 -0700

    Add model password comparison functions

--- src/auth/model.js
+++ src/auth/model.js
@@ -14,7 +14,15 @@ const userSchema = new Schema({
 userSchema.pre('save', function (next) {
   this.hashPassword(5, next);
 });
+//Does password comparison
+userSchema.statics.authenticate = function (auth) {
+  let userQuery = { username: auth.username};
 
+  return this.findOne(userQuery)
+    .then(user => user && user.comparePassword(auth.password))
+    .catch(console.error);
+};
+//Creates a more secure hashed password before saving
 userSchema.methods.hashPassword = function (salt, next) {
   bcrypt.hash(this.password, salt)
     .then(hashedPassword => {
@@ -23,9 +31,14 @@ userSchema.methods.hashPassword = function (salt, next) {
     })
     .catch(error => { throw error; });
 };
-
+//generates a token from secret and userid
 userSchema.methods.generateToken = function () {
   return jwt.sign({ id: this._id }, process.env.SECRET);
 };
+//uses bcrypt decryption to compare passwords
+userSchema.methods.comparePassword = function (password) {
+  return bcrypt.compare(password, this.password)
+    .then(valid => valid? this : null);
+}
 
 export default mongoose.model('users', userSchema);
