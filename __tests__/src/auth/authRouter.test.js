b64c5c0ecb72b46676f743dfd95200bf788a0abc
alexlhanson <alexlhanson@gmail.com>
2018-10-05 15:37:14 -0700

    Add tests and model

--- __tests__/src/auth/authRouter.test.js
+++ __tests__/src/auth/authRouter.test.js
@@ -8,8 +8,10 @@ describe('authRouter', () => {
     username: 'alex2',
     password: 'password',
   };
+  let authString = btoa(`${mockBody.username}:${mockBody.password}`);
 
-  test('POST signup request proof of life', (done) => {
+
+  test('POST register request proof of life', (done) => {
     expect.assertions(2);
     return superagent.post('http://localhost:3000/register/')
       .send(mockBody)
@@ -26,9 +28,9 @@ describe('authRouter', () => {
 
   test('GET signin request proof of life', (done) => {
     expect.assertions(2);
-    return superagent.post('http://localhost:3000/signin/')
+    return superagent.get('http://localhost:3000/signin/')
+      .set('Authorization', 'basic ' + authString)
       .send(mockBody)
-      .accept('application/JSON')
       .then(res => {
         expect(typeof res.body).toBe('object');
         expect(res.status).toBe(200);
--- src/auth/middleware.js
+++ src/auth/middleware.js
@@ -3,5 +3,32 @@
 import User from './model';
 
 export default (req, res, next) => {
+  
+  let authenticate = (auth) =>{
+    User.authenticate(auth)
+      .then(user => {
+        if(!user) {
+          throw console.error('user could not be authenticated');
+        } else {
+          req.token = user.generateToken();
+          next();
+        }
+      })
+      .catch(next);
+  };
+  
+  let auth = {};
+  let authHeader = (req.headers.authorization);
 
+  if (!authHeader) {console.error('user is not authorized');}
+
+  //basic authentication parsing 
+  if (authHeader.match(/basic/i)) {
+    let base64Header = authHeader.replace(/Basic\s+/i, '');
+    let base64Buffer = Buffer.from(base64Header, 'base64');
+    let bufferString = base64Buffer.toString();
+    let [username, password] = bufferString.split(':');
+    auth = { username, password };
+    authenticate(auth);
+  }
 };
