07576b59205d33a226e8ad79d27903ffe4f7e779
alexlhanson <alexlhanson@gmail.com>
2018-10-08 20:01:30 -0700

    Add post update for team

--- src/models/team.js
+++ src/models/team.js
@@ -2,6 +2,7 @@
 
 
 import mongoose from 'mongoose';
+import Users from '../auth/user';
 
 const Schema = mongoose.Schema;
 
@@ -15,4 +16,22 @@ const teamSchema = Schema({
   email: {type: String},
 });
 
+teamSchema.pre('save', function (req, next) {
+
+  Users.findById(req.body.user)
+    .then(user => {
+      if (!user) {
+        return Promise.reject('user not found');
+      }
+      else if (user.role === 'coach'){
+        return Promise.resolve(this.coach = req.body.user);
+      } else {
+        return Promise.resolve(this.players.push(req.body.user));
+      }
+    })
+    .then(next())
+    .catch(next);
+});
+
 export default mongoose.model('teams', teamSchema);
+
