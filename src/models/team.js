5b2f4e23ceff6625f4e46db326c51936dbf09ab7
Concross <cacrossley22@gmail.com>
2018-10-08 23:26:55 -0700

    References #48. Team GET request populates coach and player fields

--- src/models/team.js
+++ src/models/team.js
@@ -7,20 +7,26 @@ import Users from '../auth/user';
 const Schema = mongoose.Schema;
 
 const teamSchema = Schema({
-  coach: { type: Schema.Types.ObjectId, ref: 'users'},
-  players: [{ type: Schema.Types.ObjectId, ref: 'users'}],
+  coach: { type: Schema.Types.ObjectId, ref: 'users' },
+  players: [{ type: Schema.Types.ObjectId, ref: 'users' }],
   name: { type: String, required: true },
   city: { type: String, required: true },
-  state: { type: String, required: true},
+  state: { type: String, required: true },
   phone: { type: String },
-  email: {type: String},
+  email: { type: String },
 });
 
 teamSchema.pre('save', function (next) {
-  Users.findByIdAndUpdate(this.coach, {team: this._id})
+  Users.findByIdAndUpdate(this.coach, { team: this._id })
     .then(Promise.resolve(next()))
     .catch(err => Promise.reject(err));
 
+});
+
+teamSchema.pre('findOne', function (next) {
+  this.populate('coach');
+  this.populate('players');
+  next();
 });
 
 export default mongoose.model('teams', teamSchema);
