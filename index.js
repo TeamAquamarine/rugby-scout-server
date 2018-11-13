1bafcfdd383010dcd2ad126e2908f4bd9f2526b1
Concross <cacrossley22@gmail.com>
2018-11-13 10:01:26 -0800

    add seeder function to populate mongo with fake data

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 'use strict';
-
-const mongoose = require('mongoose');
 require('dotenv').config();
 require('babel-register');
+const mongoose = require('mongoose');
+const seeder = require('./src/seed');
 
 mongoose.connect(process.env.MONGODB_URI);
+seeder.seedMongo(20);
 
 require('./src/app').start(process.env.PORT);
